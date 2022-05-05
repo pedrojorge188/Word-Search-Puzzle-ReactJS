1ed59ce6219b9190305266e7a630ae8cacf739da
pedrojorge188 <pedrorodriguesjorge03@gmail.com>
2022-05-05 10:52:30 +0100

    names updated

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -7,8 +7,8 @@ function Footer(){
         <div className="card-body text-center shadow p-3 mb-5 bg-body rounded main">
             <blockquote className="blockquote mb-0"> 
             <p><b>Trabalho realizador por</b></p>
-            <footer className="blockquote-footer name-footer">Pedro Jorge a2021142041 </footer>
-            <footer className="blockquote-footer name-footer">Luiz Travassos a2021136600 </footer>
+            <footer className="name-footer">Pedro Jorge a2021142041 </footer>
+            <footer className="name-footer">Luiz Travassos a2021136600 </footer>
             </blockquote>
         </div>
         </div>
