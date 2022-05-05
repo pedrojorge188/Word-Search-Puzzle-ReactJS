37a486e02093c1eeb7480454076fd4c0f2bda4d5
pedrojorge188 <pedrorodriguesjorge03@gmail.com>
2022-05-05 09:51:56 +0100

    generated project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./assets/styles/App.css";
+import React from "react";
+
+
+function App() {
+  return (
+    <div id="container">
+      <React.StrictMode>
+      
+      </React.StrictMode>
+    </div>
+
+  );
+}
+
+export default App;
+// Esta linha também poderia ser eliminada
+// e adefinição da funsão ser substituida 
+// export default function App() {
