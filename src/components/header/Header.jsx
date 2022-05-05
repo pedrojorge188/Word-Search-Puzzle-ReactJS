6b79c8368186212889ef6d47ae29a3c2b19878e5
Luyiz333 <luistrvassos03@gmail.com>
2022-05-05 10:47:38 +0100

    Boas

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ function Header(){
         <nav className="navbar navbar-light bg-light">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">
-                    <img src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width={30} height={24} className="d-inline-block align-text-top"/>
+                    <img src="public\Images\HeaderImage.jpg" alt="" width={30} height={24} className="d-inline-block align-text-top"/>
                     <h1>Sopa de Letras</h1>
                 </a>
             </div>
